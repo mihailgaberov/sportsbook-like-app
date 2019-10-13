2b7ea53667d74c24b9d6e89ddbbf21ae2531d5db
mihailgaberov <mihail.gaberov@gmail.com>
2019-10-13 14:12:08 +0200

    Styled Market names.

--- src/components/Market/Market.tsx
+++ src/components/Market/Market.tsx
@@ -1,11 +1,20 @@
 import * as React from 'react';
 import Selection from "../Selection";
+import { makeStyles } from "@material-ui/core";
+
+const useStyles = makeStyles({
+  marketName: {
+    color: 'lightgrey',
+  }
+});
+
 
 export default function Market({name, selections = []}: {name: string, selections: ISelectionType[]}) {
+  const classes = useStyles();
 
   return (
     <React.Fragment>
-      <h3>{name}</h3>
+      <h3 className={classes.marketName}>{name}</h3>
       {selections.map(sel => <Selection key={sel.id} name={sel.name} price={sel.price} />)}
     </React.Fragment>
   );
