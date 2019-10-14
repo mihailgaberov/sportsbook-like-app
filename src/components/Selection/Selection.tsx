d92f2176da74a89560da0119bb795ed48bc17cc5
mihailgaberov <mihail.gaberov@gmail.com>
2019-10-14 14:27:44 +0200

    Respect the selected states from local storage as well.

--- src/components/Selection/Selection.tsx
+++ src/components/Selection/Selection.tsx
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import { useMachine } from '@xstate/react';
 import { bettingMachine } from '../../state-machines/betting-machine';
+import { readRecord } from "../../utilities/localStorageService";
 
 const useStyles = makeStyles({
   buttonLabel: {
@@ -19,21 +20,25 @@ const useStyles = makeStyles({
     padding: '4px 14px',
     textAlign: 'center',
   },
+  selectionAdded: {
+    backgroundColor: '#00d29b',
+  }
 });
 
 export default function Selection({ data }: { data: ISelectionType }) {
   const classes = useStyles();
   const [current, send] = useMachine(bettingMachine);
 
-  function makeSelection() {
+  const makeSelection = () => {
     send('TOGGLE_SELECTION', { data });
-  }
+  };
+
+  const isSelected = (selectionId: string) => readRecord(selectionId) || current.matches('success');
 
   return (
-    <Button className={classes.selection} classes={{ label: classes.buttonLabel }} onClick={makeSelection}>
+    <Button className={classes.selection} classes={{ label: classes.buttonLabel, root: isSelected(data.id) ? classes.selectionAdded : ''}} onClick={makeSelection}>
       <h5 className={classes.name}>{data.name}</h5>
       <span>{data.price}</span>
-      {current.matches('success') ? 'on' : 'off'}
     </Button>
   );
 }
