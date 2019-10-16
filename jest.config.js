7fd105a92a7f1287875dbbf4244026265587f18f
mihailgaberov <mihail.gaberov@gmail.com>
2019-10-16 14:40:32 +0200

    Configured jest and RTL.

--- jest.config.js
+++ jest.config.js
@@ -12,10 +12,7 @@ module.exports = {
   // Runs special logic, such as cleaning up components
   // when using React Testing Library and adds special
   // extended assertions to Jest
-  setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect"
-  ],
+  setupTestFrameworkScriptFile: "@testing-library/jest-dom/extend-expect.js",
 
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
