b8133a2ff068881ee3501cf3edd20abb6a2e83f2
mihailgaberov <mihail.gaberov@gmail.com>
2019-10-16 09:31:39 +0200

    Added unit tests to Betslip component.

--- src/components/Betslip/Betslip.test.tsx
+++ src/components/Betslip/Betslip.test.tsx
@@ -1,13 +1,14 @@
-import * as React from 'react';
+import { Drawer } from '@material-ui/core';
+import { shallow } from "enzyme";
+import * as React from "react";
+import Betslip from "./Betslip";
 
-import { shallow } from 'enzyme';
-import Betslip from './Betslip';
 
 const localStorageMock = (() => {
   let store = {};
   return {
     getItem: (key: string | number) => store[key],
-    setItem: (key: string | number, value: { toString: () => void; }) => {
+    setItem: (key: string | number, value: { toString: () => void }) => {
       store[key] = value.toString();
     },
     clear: () => {
@@ -15,19 +16,27 @@ const localStorageMock = (() => {
     },
     removeItem: (key: string | number) => {
       delete store[key];
-    }
+    },
+    readAllFromStorage: () => [
+      { id: "SEL_1", name: "Man Utd to WIN", price: 1.23 }
+    ]
   };
 })();
 
-Object.defineProperty(window, 'localStorage', { value: localStorageMock });
+Object.defineProperty(window, "localStorage", { value: localStorageMock });
 
 const setup = () => {
-  return shallow(<Betslip removeSelectionCallback={jest.fn} />)
+  return shallow(<Betslip removeSelectionCallback={jest.fn} />);
 };
 
-describe('Betslip', () => {
-  it('renders without crashing', () => {
+describe("Betslip", () => {
+  it("renders without crashing", () => {
     const wrapper = setup();
     expect(wrapper).not.toBe(null);
+  });
+
+  it("should be able to render drawer", () => {
+    const wrapper = setup();
+    expect(wrapper.find(Drawer).length).toBe(1);
   });
 });
