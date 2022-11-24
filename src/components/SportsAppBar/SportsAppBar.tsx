955433d83037fc14e96e24626f0f412c73869bef
Mihail Gaberov <mihail.gaberov@olx.com>
2022-11-24 15:02:59 +0100

    :sparkles: Updated the logo

--- src/components/SportsAppBar/SportsAppBar.tsx
+++ src/components/SportsAppBar/SportsAppBar.tsx
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
-import logo from '../../images/logo.png';
+import logo from './logo.svg';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
